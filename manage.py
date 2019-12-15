cb0c926f9259c0ab4fbb19a99c0adf13f0bec751
Dbaelvis <dbaelvis@gmail.com>
2019-12-15 21:21:32 -0200

    primeiro commit 15-12-2019 3

--- manage.py
+++ manage.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+
+'''
 import os
 import sys
 
@@ -13,3 +15,23 @@ if __name__ == "__main__":
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+'''
+import os
+import sys
+
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestao_clientes.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
